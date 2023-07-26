63e3647b802c879a3ba1e9730b04e4212839d227
Nathaniel81 <104891061+Nathaniel81@users.noreply.github.com>
2023-07-26 21:16:44 +0300

    Create 3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Module: 3-lru_cache.py
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU(List Recently Used) Caching"""
+    lst = []
+
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Stores a new item to the cache"""
+        if key is not None and item is not None:
+            if key in LRUCache.lst:
+                LRUCache.lst.remove(key)
+                LRUCache.lst.append(key)
+                return
+            self.cache_data[key] = item
+            LRUCache.lst.append(key)
+
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+            key_to_be_removed = LRUCache.lst[0]
+            if (key_to_be_removed in self.cache_data.keys()):
+                self.cache_data.pop(key_to_be_removed)
+                print('DISCARD: {}'.format(LRUCache.lst.pop(0)))
+
+    def get(self, key):
+        """Returns an item based on the key"""
+        if (key is None or key not in self.cache_data.keys()):
+            return None
+        if key in LRUCache.lst:
+            LRUCache.lst.remove(key)
+            LRUCache.lst.append(key)
+
+        return self.cache_data[key]
