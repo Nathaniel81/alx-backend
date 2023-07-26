7200b834b8f963c33dfc51d6f7fdfd7a6df5418d
Nathaniel81 <104891061+Nathaniel81@users.noreply.github.com>
2023-07-26 21:22:17 +0300

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,40 +1,56 @@
 #!/usr/bin/env python3
 """
-Module: 4-mru_cache.py
+module: LFUCache
 """
-from base_caching import BaseCaching
+from collections import defaultdict
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
-class MRUCache(BaseCaching):
-    """MRU(Most Recently Used) Cache"""
-    lst = []
+class LFUCache(BaseCaching):
+    """LFUCache Class
+    This is a caching class that uses LRU Algorithm
+    """
 
-    def __init__(self):
-        """Initialization"""
+    def __init__(self) -> None:
+        """Initializes the LFUCache with a list of empty keys and dictionary
+        """
         super().__init__()
+        self.keys = []
+        self.frequency = defaultdict(lambda: 0)
 
     def put(self, key, item):
-        """Stores a new item to the cache"""
-        if key is not None and item is not None:
-            if key in MRUCache.lst:
-                MRUCache.lst.remove(key)
-                MRUCache.lst.append(key)
-                return
-            self.cache_data[key] = item
-            MRUCache.lst.append(key)
+        """Saves the item to the cache with the given key
+        """
+        if key and item:
+            if key not in self.keys:
+                if len(self.keys) == self.MAX_ITEMS:
+                    min_freq = min(self.frequency.values())
+                    l_freq = list(
+                        filter(lambda item: item[1] == min_freq,
+                               self.frequency.items()))
+                    if len(l_freq) == 1:
+                        rm_key = l_freq[0][0]
+                    else:
+                        idxs = sorted([(self.keys.index(k[0]), k[0])
+                                       for k in l_freq])
+                        rm_key = idxs[0][1]
+                    self.keys.remove(rm_key)
+                    self.frequency.pop(rm_key)
+                    self.cache_data.pop(rm_key)
+                    print(f'DISCARD: {rm_key}')
 
-        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
-            key_to_be_removed = MRUCache.lst[-2]
-            if (key_to_be_removed in self.cache_data.keys()):
-                self.cache_data.pop(key_to_be_removed)
-                print('DISCARD: {}'.format(MRUCache.lst.pop(-2)))
+                self.keys.append(key)
+
+            self.frequency[key] += 1
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Returns an item based on the key"""
-        if (key is None or key not in self.cache_data.keys()):
-            return None
-        if key in MRUCache.lst:
-            MRUCache.lst.remove(key)
-            MRUCache.lst.append(key)
-
-        return self.cache_data[key]
+        """Returns the value in self.cache_data linked to key or None if key
+        is None or doesn't exist in self.cache_data
+        """
+        if key in self.cache_data:
+            self.frequency[key] += 1
+            self.keys.remove(key)
+            self.keys.append(key)
+        return self.cache_data.get(key)
