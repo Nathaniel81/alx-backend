6a2877d1adc1de77e5c9574a098f03961d7a0d69
Nathaniel81 <104891061+Nathaniel81@users.noreply.github.com>
2023-07-26 21:19:06 +0300

    Create 100-lfu_cache.py

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Module: 4-mru_cache.py
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU(Most Recently Used) Cache"""
+    lst = []
+
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Stores a new item to the cache"""
+        if key is not None and item is not None:
+            if key in MRUCache.lst:
+                MRUCache.lst.remove(key)
+                MRUCache.lst.append(key)
+                return
+            self.cache_data[key] = item
+            MRUCache.lst.append(key)
+
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+            key_to_be_removed = MRUCache.lst[-2]
+            if (key_to_be_removed in self.cache_data.keys()):
+                self.cache_data.pop(key_to_be_removed)
+                print('DISCARD: {}'.format(MRUCache.lst.pop(-2)))
+
+    def get(self, key):
+        """Returns an item based on the key"""
+        if (key is None or key not in self.cache_data.keys()):
+            return None
+        if key in MRUCache.lst:
+            MRUCache.lst.remove(key)
+            MRUCache.lst.append(key)
+
+        return self.cache_data[key]
