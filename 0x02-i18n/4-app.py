a7965c2a34f54ad41c843b796ce3a4aa61eab086
Nathaniel81 <104891061+Nathaniel81@users.noreply.github.com>
2023-08-01 22:30:13 +0300

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,37 +1,36 @@
 #!/usr/bin/env python3
-"""
-Module: 4-app.py
-"""
+"""A simple Flask app"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+app = Flask(__name__)
+babel = Babel(app)
 
-class Config(object):
-    """Config class for Babel configuration"""
+
+class Config:
+    """A Config app class"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
+
+
+@app.route('/')
+def welcome() -> str:
+    """Renders a message"""
+    return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """Get the local language"""
-    lcl = request.args.get('locale', None)
-    if lcl and lcl in app.config['LANGUAGES']:
-        return lcl
+    """Get locale from request"""
+    lang = request.args.get('locale')
+    if lang in Config.LANGUAGES:
+        return lang
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def home():
-    """Home page"""
-    return render_template('4-index.html')
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
